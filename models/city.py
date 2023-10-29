f73d11c49f597852ca6c7d2b2b1a1cbf02e6badf
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-10-28 22:16:40 -0700

    updated

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -15,4 +16,6 @@ class City(BaseModel, Base):
 
     # Column for state ID, a foreign key to states.id, not nullable
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="all, delete"
+
 
--- models/place.py
+++ models/place.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, ForeignKey, Float
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = "places"
+
+    city_id = Column(String(60), Nullable=False, ForeignKey('cities.id'))
+    user_id = Column(String(60), Nullable=False, ForeignKey('uses.id'))
+    name = Column(String(128), Nullable=False)
+    description = Column(String(1024), Nullable=True)
+    number_rooms = Column(Integer, Nullable=False, Default=0)
+    number_bathrooms =  Column(Integer, Nullable=False, Default=0)
+    max_guest =  Column(Integer, Nullable=False, Default=0)
+    price_by_night =   Column(Integer, Nullable=False, Default=0)
+    latitude =  Column(Float, Nullable=True)
+    longitude =  Column(Float, Nullable=True)
     amenity_ids = []
